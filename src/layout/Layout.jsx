27e73e3608474398594dd345c5757c5a409317e3
Kappo <andrea30.4.99@gmail.com>
2024-03-03 16:18:11 +0100

    Update Layout.jsx

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
   return (
     <div className="w-full h-dvh relative overflow-hidden">
       <div className="fixed top-0 left-0 w-full h-80 bg-ruta_purple -z-10"></div>
-      <main className="w-full h-full pt-8 pb-16 px-4 overflow-y-auto">
+      <main className="w-full h-full pt-8 pb-20 px-4 overflow-y-auto">
         {children}
       </main>
       <BottomNavigation />
