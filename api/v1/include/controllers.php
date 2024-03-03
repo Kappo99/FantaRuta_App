02e6618d2872175d6657c168c2824688ce1e0f6f
Kappo <andrea30.4.99@gmail.com>
2024-03-04 00:08:11 +0100

    api rutazioni

--- api/v1/include/controllers.php
+++ api/v1/include/controllers.php
@@ -2,3 +2,4 @@
 
 require_once SERVER_ROOT . 'controllers/auth.php';
 require_once SERVER_ROOT . 'controllers/rutatori.php';
+require_once SERVER_ROOT . 'controllers/rutazioni.php';
--- api/v1/include/model.php
+++ api/v1/include/model.php
@@ -2,3 +2,4 @@
 
 require_once SERVER_ROOT . 'model/Log.php';
 require_once SERVER_ROOT . 'model/Rutatore.php';
+require_once SERVER_ROOT . 'model/Rutazione.php';
