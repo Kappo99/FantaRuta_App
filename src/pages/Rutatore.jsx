42d70e4e382884be2c2ba3fa079b4ea56e2ddc6c
Kappo <andrea30.4.99@gmail.com>
2024-03-03 15:45:27 +0100

    Update Rutatore.jsx

--- src/pages/Rutatore.jsx
+++ src/pages/Rutatore.jsx
@@ -2,7 +2,10 @@
 export default function Rutatore() {
     return (
         <>
-        <h1 className="h1">Rutatore</h1>
+            <h1 className="h1">Rutatore</h1>
+            <p>Questa sezione è ancora in fase di sviluppo</p>
+            <p>L'idea è quella di mostrare le Rutazioni scelte della settimana, e lo storico di tutte le Rutazioni scelte in passato</p>
+            <p>Se hai dei suggeerimenti scrivici!</p>
         </>
     );
 }
