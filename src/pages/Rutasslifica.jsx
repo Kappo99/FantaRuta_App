53e54f0c33b901da4cd29b5453a2d6410e21d5d2
Kappo <andrea30.4.99@gmail.com>
2024-03-03 16:11:08 +0100

    white

--- src/pages/Rutasslifica.jsx
+++ src/pages/Rutasslifica.jsx
@@ -3,8 +3,8 @@ import Classifica from "../components/Classifica";
 export default function Rutasslifica() {
     return (
         <>
-            <h1 className="h1">Rutasslifica</h1>
-            <h2 className="h3 text-center">Giornata 3</h2>
+            <h1 className="h1 text-white">Rutasslifica</h1>
+            <h2 className="h3 text-center text-white">Giornata 3</h2>
             <Classifica />
         </>
     );
--- src/pages/Rutatore.jsx
+++ src/pages/Rutatore.jsx
@@ -2,10 +2,10 @@
 export default function Rutatore() {
     return (
         <>
-            <h1 className="h1">Rutatore</h1>
-            <p>Questa sezione è ancora in fase di sviluppo</p>
-            <p>L'idea è quella di mostrare le Rutazioni scelte della settimana, e lo storico di tutte le Rutazioni scelte in passato</p>
-            <p>Se hai dei suggerimenti scrivici!</p>
+            <h1 className="h1 text-white">Rutatore</h1>
+            <p className="text-white">Questa sezione è ancora in fase di sviluppo</p>
+            <p className="text-white">L'idea è quella di mostrare le Rutazioni scelte della settimana, e lo storico di tutte le Rutazioni scelte in passato</p>
+            <p className="text-white">Se hai dei suggerimenti scrivici!</p>
         </>
     );
 }
