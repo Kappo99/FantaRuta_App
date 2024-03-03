14d1179d56f0e3185b81a0a51256b2f2b7dcbdf7
Kappo <andrea30.4.99@gmail.com>
2024-03-03 23:25:46 +0100

    Bonus

--- src/components/CardRutazione.jsx
+++ src/components/CardRutazione.jsx
@@ -14,18 +14,28 @@ export default function CardRutazione(props) {
                         <b>Malus:</b> {props.malusText}
                     </p>
                 }
+                {props.bonusText &&
+                    <p className="card__bonus-text">
+                        <b>Bonus:</b> {props.bonusText}
+                    </p>
+                }
             </div>
             <div className="col-span-1 flex flex-col items-center justify-center">
                 <div className="card__rutas">
                     {props.rutas}R
                 </div>
                 <div className="flex items-end gap-2 mt-2">
-                    <div className="card__bonus">
-                        +{props.bonus}
+                    <div className="card__monteruta">
+                        +{props.monteruta}
                     </div>
-                    {props.malus != 0 &&
+                    {props.malus != null &&
                         <div className="card__malus">
                             -{props.malus}
+                        </div>
+                    }
+                    {props.bonus != null &&
+                        <div className="card__bonus">
+                            +{props.bonus}
                         </div>
                     }
                 </div>
