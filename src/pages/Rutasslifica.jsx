472b29fca46d441394fa5992ed68574cc0c561ab
Kappo <andrea30.4.99@gmail.com>
2024-03-05 01:10:56 +0100

    Classifica

--- src/pages/Rutasslifica.jsx
+++ src/pages/Rutasslifica.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import Classifica from "../components/Classifica";
 import { GIORNATA } from "../utilities/Constants";
 import callApi from "../hooks/callApi";
 
@@ -28,7 +27,6 @@ export default function Rutasslifica() {
 		<>
 			<h1 className="h1 text-white">Rutasslifica</h1>
 			<h2 className="h3 text-center text-white">Giornata {GIORNATA}</h2>
-			{/* <Classifica /> */}
 			<table className="classifica">
 				<thead>
 					<tr>
@@ -48,28 +46,6 @@ export default function Rutasslifica() {
 				</tbody>
 			</table>
 			{rutasslificaList.length == 0 && <p className="text-white text-center">Rutasslifica non disponibile...</p>}
-			{/* {[
-						[3, 'Stella', 1],
-						[2, 'Cardazzo', 9],
-						[13, 'Cappone', 75],
-						[9, 'Mantovan', 55],
-						[7, 'Tognella', 45],
-						[15, 'Peruzzotti', 4],
-						[21, 'Longobardi', 4],
-						[23, 'Ferri', 3],
-						[10, 'Losi', 2],
-						[18, 'Puricelli', 15],
-						[19, 'Siviero', 15],
-						[12, 'Sala', 5],
-						[6, 'Bozzano',],
-						[8, 'Scarabelli',],
-					].map(([maglia, rutatore, monteruta], index) => (
-						<tr key={maglia}>
-							<td>{index + 1}</td>
-							<td>#{maglia} {rutatore}</td>
-							<td>{monteruta}</td>
-						</tr>
-					))} */}
 		</>
 	);
 }
