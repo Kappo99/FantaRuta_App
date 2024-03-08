c961e546c7cddaf5c44b180d3c933e0f61175bb4
Kappo <andrea30.4.99@gmail.com>
2024-03-08 12:36:59 +0100

    Update Layout.jsx

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 export default function Layout({ children }) {
   return (
     <div className="w-full h-dvh relative pb-8">
-      <div className="fixed top-0 left-0 w-full h-1/3 bg-ruta_purple -z-10"></div>
+      <div className="fixed top-0 left-0 w-full h-2/5 bg-ruta_purple -z-10"></div>
       <Navbar />
       <main className="pt-20 px-4">
         {children}
