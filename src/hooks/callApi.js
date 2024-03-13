88fe0013771bb1b82a86f7648b0791d18fbe0e08
Kappo <andrea30.4.99@gmail.com>
2024-03-13 19:01:48 +0100

    Update callApi.js

--- src/hooks/callApi.js
+++ src/hooks/callApi.js
@@ -17,6 +17,7 @@ async function callApi(uri, method, data) {
 
   if (response.status == 401) {
     localStorage.removeItem("token");
+    localStorage.removeItem("idRutatore");
   }
 
   return response;
