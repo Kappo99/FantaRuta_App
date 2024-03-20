49c2079c350ca802d561f9444d9935f68b169a91
Kappo <andrea30.4.99@gmail.com>
2024-03-20 13:44:54 +0100

    v1.3.4 (days)

--- src/utilities/Constants.jsx
+++ src/utilities/Constants.jsx
@@ -1,8 +1,8 @@
 export const START_DATE = new Date('2024-02-18');
 export const TODAY = new Date();
 export const IS_EDITABLE = TODAY.getDay() == 0;
-const dateDiff = new Date(TODAY.getTime() - START_DATE.getTime());
-const daysDiff = dateDiff.getUTCDate() - 1;
+const dateDiff = TODAY.getTime() - START_DATE.getTime(); // Calcolo della differenza in millisecondi
+const daysDiff = Math.floor(dateDiff / (1000 * 3600 * 24)); // Convertire la differenza da millisecondi a giorni
 export const GIORNATA = Math.floor(daysDiff / 7) + 1;
 
 export const MAX_RUTAS = 50;
