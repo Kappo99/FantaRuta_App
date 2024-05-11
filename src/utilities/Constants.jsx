771e242c506e2263d81a0bdb7e8d816b1ff4d580
Kappo <andrea30.4.99@gmail.com>
2024-05-11 12:00:34 +0200

    v1.5.5 (FINE)

--- src/utilities/Constants.jsx
+++ src/utilities/Constants.jsx
@@ -1,9 +1,9 @@
 export const START_DATE = new Date('2024-02-18');
 export const TODAY = new Date();
-export const IS_EDITABLE = TODAY.getDay() == 0;
+export const IS_EDITABLE = false; //TODAY.getDay() == 0; //* campionato terminato il 12-05-2024
 const dateDiff = TODAY.getTime() - START_DATE.getTime(); // Calcolo della differenza in millisecondi
 const daysDiff = Math.floor(dateDiff / (1000 * 3600 * 24)); // Convertire la differenza da millisecondi a giorni
-export const GIORNATA = Math.floor(daysDiff / 7) + 1;
+export const GIORNATA = 12; //Math.floor(daysDiff / 7) + 1; //* campionato terminato il 12-05-2024
 
 export const MAX_RUTAS = 50;
 
